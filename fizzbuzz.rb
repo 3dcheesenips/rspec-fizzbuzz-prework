03b30aca8cd6dd136e7ff5c9d1b1d285cc75866c
Maharshee roy <m.roy@optonline.net>
2018-10-31 17:46:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,2 @@
-def fizzbuzz(arg)
-  if arg % 3 === 0 
-    return "Fizz"
-  if arg % 5 === 0 
-    return "Buzz"
-  if arg % 15 === 0 
-    return "FizzBuzz"
-  else
-    return nil
+def fizzbuzz
 end
